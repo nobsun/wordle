5917bd6bb7a1c19df897c1e6a7ed0c3f0b4be016
Nobuo Yamashita <nobsun@sampou.org>
2022-05-07 08:31:37 +0900

    modify slightly

--- src/Wordle.hs
+++ src/Wordle.hs
@@ -10,13 +10,6 @@ import Data.Maybe
 import Interact
 import Debug.Trace
 
-{- | 
-任意の入力列をそれぞれの入力文字列を"なんか関数"に変換
->>> putStr $ unlines $ wordle undefined ["Hi.", "お元気ですか？"]
-なんか関数
-なんか関数
--}
-
 wordle :: String -> [String] -> [String]
 wordle extra = mapMaybe output . eval . initial extra
 
@@ -66,9 +59,15 @@ step state = case state of
                             guess:pattern:_ = words i
                             dict' = buildFilter guess pattern dict
 
-matchPat :: String -> String -> String
-matchPat guess str
-    = case zipWith phi guess str of
+buildFilter :: String -> String -> [String] -> [String]
+buildFilter _     ""  = id
+buildFilter guess pattern = filter p
+    where
+        p w = pattern == matchPattern guess w
+
+matchPattern :: String -> String -> String
+matchPattern guess target
+    = case zipWith phi guess target of
         str1 -> zipWith (psi str1) guess str1
     where
         phi x y = bool y ' ' (x == y)
@@ -76,24 +75,3 @@ matchPat guess str
             | y == ' '     = 'g'
             | x `elem` zs  = 'y'
             | otherwise    = 'b'
-
-buildFilter :: String -> String -> [String] -> [String]
-buildFilter _     ""  = id
-buildFilter guess pat = filter p
-    where
-        p w = pat == matchPat guess w
-
--- matchPattern :: String -> String -> String
--- matchPattern guess target = case zipWith phi guess target of
---     str -> zipWith (psi str) guess str
---     where
---         phi x y = bool y  ' ' (x == y)
---         psi zs x y
---             | y == ' '     = 'g'
---             | x `elem` zs  = 'y'
---             | otherwise    = 'b'
-
--- buildFilter :: String -> String -> [String] -> [String]
--- buildFilter guess pattern = filter p
---     where
---         p w = pattern == matchPattern guess pattern 
